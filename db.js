a735e1e91388ded54b010b9871f9491b9b710546
quang-kiet.tran <quang-kiet.tran@ekino.com>
2018-05-03 18:09:56 +0700

    create authentication restfull api with nodejs

--- db.js
+++ db.js
@@ -1,2 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb://shoppyAuth:shoppyauth123@ds143892.mlab.com:43892/shoppy-auth')
+	.then(() => {
+		console.log('Database connection successful')
+	})
+	.catch(err => {
+		console.error(err)
+	})
--- user/UserController.js
+++ user/UserController.js
@@ -9,14 +9,14 @@ var User = require('./User');
 // CREATES A NEW USER
 router.post('/', function (req, res) {
     User.create({
-            name : req.body.name,
-            email : req.body.email,
-            password : req.body.password
-        }, 
-        function (err, user) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(user);
-        });
+		name : req.body.name,
+		email : req.body.email,
+		password : req.body.password
+	},
+	function (err, user) {
+		if (err) return res.status(500).send("There was a problem adding the information to the database.");
+		res.status(200).send(user);
+	});
 });
 
 // RETURNS ALL THE USERS IN THE DATABASE
